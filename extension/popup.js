e88a901c5690d829ef46815e935b3abc28d70cba
benvh <ben.vanhoutven@gmail.com>
2017-04-09 22:58:11 +0200

    es6 syntax
    chrome supports some useful es6 syntax features, let's make use of them.
    Also added a popup.css file and some "rendering" utility functions

--- extension/popup.js
+++ extension/popup.js
@@ -7,9 +7,17 @@
      * @returns {Promise} resolves to the active tab's url hostname
      */
     function FetchActiveTabHostname() {
-        return new Promise(function(resolve, reject) {
-            chrome.tabs.executeScript({ code: 'location.host' }, function(result) {
-                resolve(result[0].replace(/^www\./, ''));
+        return new Promise((resolve, reject) => {
+            chrome.tabs.executeScript({ code: 'location.host' }, result =>{
+                if(result) {
+                    resolve(result[0].replace(/^www\./, ''));
+                } else {
+                    if(chrome.runtime.lastError) {
+                        reject(chrome.runtime.lastError.message);
+                    } else {
+                        reject('fatal error');
+                    }
+                }
             }); 
         });
     }
@@ -21,8 +29,8 @@
      * @returns {Promise} resolves to the first response received after sending the message
      */
     function SendNativeMessage(message) {
-        return new Promise(function(resolve, reject) {
-            chrome.runtime.sendNativeMessage('tech.benvh.chrome_pass', message, function(response) {
+        return new Promise((resolve, reject) => {
+            chrome.runtime.sendNativeMessage('tech.benvh.chrome_pass', message, response => {
                 if(response == undefined || response.kind === 'error') {
                     reject(response);
                 } else {
@@ -40,10 +48,8 @@
      * @returns {Promise} resolve's to the user's password or throws an error if an error occured
      */
     function FetchUserPasssword(hostname, username) {
-        return SendNativeMessage({ action: "fetch_user_password", arguments: { hostname: hostname, username: username } })
-            .then(function(response) {
-                return response.data.password.trim();
-            });
+        return SendNativeMessage({ action: 'fetch_user_password', arguments: { hostname: hostname, username: username } })
+            .then(response => response.data.password.trim());
     }
 
     /**
@@ -51,35 +57,25 @@
      * @returns {Promise}
      */
     function ListUsernamesForHostname(hostname) {
-        return SendNativeMessage({ action: "list_host_usernames", arguments: { hostname: hostname } })
-            .then(function(response) {
-                return response.data.usernames;
-            })
-            .then(function(usernames) {
-                usernames.forEach(function(username) {
-                    var div = document.createElement('div');
+        return SendNativeMessage({ action: 'list_host_usernames', arguments: { hostname: hostname } })
+            .then(response => response.data.usernames)
+            .then(usernames => {
+                usernames.forEach(username => {
+                    const div = document.createElement('div');
                     div.classList.add('username-entry');
                     div.textContent = username;
                     div.addEventListener('click', HandleHostUsernameClick.bind(undefined, hostname, username));
                     document.body.appendChild(div);
                 });
             })
-            .catch(function(error) {
-                // TODO: Proper error handling
-                var div = document.createElement('div');
-                div.textContent = "No users found for " + hostname;
-                document.body.appendChild(div);
-            });
+            .catch(error => ShowError(`No users found for ${hostname}`));
     }
 
     /**
      * Click event handler for the username list entries shown in the extension popup.
      */
     function HandleHostUsernameClick(hostname, username) {
-        FetchUserPasssword(hostname, username)
-            .then(function(password) {
-                InjectUsernameAndPassword(username, password);
-            });
+        FetchUserPasssword(hostname, username).then(password => InjectUsernameAndPassword(username, password));
     }
 
     /**
@@ -91,15 +87,37 @@
         });
     }
 
+    /**
+     * Utility method to render a username list
+     */
+    function ShowUsernames(usernames) {
+
+    }
+
+    /**
+     * Utility method to show errors
+     */
+    function ShowError(message) {
+        const errDiv = document.createElement('div');
+        const errSad = document.createElement('div');
+        errSad.classList.add('error-sad');
+        errSad.textContent = ':(';
+
+        errDiv.classList.add('error');
+        errDiv.textContent = message;
+
+        document.body.appendChild(errSad);
+        document.body.appendChild(errDiv);
+    }
+
 
     /**
      * Automatically list all available users when the popup is opened
      */
     global.addEventListener("load", function() {
         FetchActiveTabHostname()
-            .then(function(hostname) {
-                ListUsernamesForHostname(hostname);
-            });
+            .then(hostname => ListUsernamesForHostname(hostname))
+            .catch(message => ShowError('Not available for this tab'));
 
     });
 
